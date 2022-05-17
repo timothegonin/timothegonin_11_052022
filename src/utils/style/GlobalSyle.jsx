6935fe66852d2d820f1923b89d1baeea2d4310aa
TimotheGonin <gonin.timothe@gmail.com>
2022-05-17 15:41:23 +0200

    🔧 adding basic layout anf styles

--- src/utils/style/GlobalSyle.jsx
+++ src/utils/style/GlobalSyle.jsx
@@ -4,6 +4,20 @@ const StyledGlobalStyle = createGlobalStyle`
   *{
   font-family: 'Montserrat', sans-serif;
   }
-  
+  *,
+  ::before,
+  ::after {
+      box-sizing: border-box;
+      padding: 0;
+      margin: 0;
+  }
+  html{
+    font-size:62.5%;
+    max-width: 144rem;
+  }
+  body{
+    padding:0 10rem;
+  }
+
 `
 export default StyledGlobalStyle

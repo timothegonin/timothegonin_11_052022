451e66d8497ec28ea24d7f1a65e17e58bc9cf305
TimotheGonin <gonin.timothe@gmail.com>
2022-05-17 11:09:45 +0200

    🚧 Routes config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import Home from './pages/Home'
+import Rental from './pages/Rental'
+import About from './pages/About'
+import Error from './pages/Error'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="rental" element={<Rental />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </React.StrictMode>
+  </BrowserRouter>
 )

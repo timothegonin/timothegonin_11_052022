fad2290a927e4a7fc2850ce849b9becad123a4f5
TimotheGonin <gonin.timothe@gmail.com>
2022-05-19 10:07:52 +0200

    💄 responsive banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -3,6 +3,7 @@ import Background from '../../assets/home-banner.jpg'
 
 const StyledBanner = styled.section`
   max-width: 1240px;
+  width: 100%;
   height: 223px;
   border-radius: 2.5rem;
   display: flex;
@@ -18,6 +19,19 @@ const StyledBanner = styled.section`
     color: white;
     font-weight: 500;
     font-size: 4.8rem;
+  }
+
+  @media screen and (max-width: 992px) {
+    height: 11.1rem;
+    border-radius: 1rem;
+    margin-top: 1.6rem;
+    h2 {
+      font-weight: 500;
+      font-size: 24px;
+      line-height: 100%;
+      margin-left: 1.6rem;
+      margin-right: 10.2rem;
+    }
   }
 `
 

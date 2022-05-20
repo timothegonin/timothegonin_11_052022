c012aa989f8d37584a9509388b6ef23ee191bfbe
TimotheGonin <gonin.timothe@gmail.com>
2022-05-20 10:36:54 +0200

    🚧 error page style and layout

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,4 +1,40 @@
+import { Link } from 'react-router-dom'
+import colors from '../../utils/style/colors'
+import ErrorImg from '../../assets/404.svg'
+import styled from 'styled-components'
+
+const ErrorWrapper = styled.div`
+  text-align: center;
+`
+
+const StyledErrorImg = styled.img`
+  height: 26.3rem;
+  width: 59.7rem;
+  margin-top: 16.9rem;
+`
+const ErrorMessage = styled.p`
+  color: ${colors.primary};
+  font-weight: 500;
+  font-size: 3.6rem;
+  margin-top: 6.6rem;
+`
+const ErrorLink = styled(Link)`
+  color: ${colors.primary};
+  font-weight: 500;
+  font-size: 18px;
+  display: inline-block;
+  margin-top: 18.2rem;
+`
+
 const Error = () => {
-  return <h2>Error</h2>
+  return (
+    <ErrorWrapper>
+      <StyledErrorImg src={ErrorImg} alt="404 illustration" />
+      <ErrorMessage>
+        Oups ! La page que vous demandez n'existe pas.
+      </ErrorMessage>
+      <ErrorLink to="/">Retourner sur la page d'accueil</ErrorLink>
+    </ErrorWrapper>
+  )
 }
 export default Error

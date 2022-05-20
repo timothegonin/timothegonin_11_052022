28d7123f1c6114428cef62535beacdf139f943f6
TimotheGonin <gonin.timothe@gmail.com>
2022-05-20 09:51:58 +0200

    ⚡️ Adjust router content

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import Header from '../../components/Header'
 const About = () => {
   return (
     <React.Fragment>
-      <Header />
       <h2>About</h2>
     </React.Fragment>
   )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
-import Header from '../../components/Header'
 import Banner from '../../components/Banner'
 import Gallery from '../../components/Gallery'
-import Footer from '../../components/Footer'
 
 const Home = () => {
   return (
     <React.Fragment>
-      <Header />
       <Banner />
       <Gallery />
-      <Footer />
     </React.Fragment>
   )
 }

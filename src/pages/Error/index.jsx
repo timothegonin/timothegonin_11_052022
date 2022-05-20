9d0f16ccb1012ad14fec578350a8bd8377530e17
TimotheGonin <gonin.timothe@gmail.com>
2022-05-20 10:45:41 +0200

    💄 reponsive error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -11,12 +11,21 @@ const StyledErrorImg = styled.img`
   height: 26.3rem;
   width: 59.7rem;
   margin-top: 16.9rem;
+  @media screen and (max-width: 768px) {
+    height: 9.9rem;
+    width: 19.8rem;
+    margin-top: 19.5rem;
+  }
 `
 const ErrorMessage = styled.p`
   color: ${colors.primary};
   font-weight: 500;
   font-size: 3.6rem;
   margin-top: 6.6rem;
+  @media screen and (max-width: 768px) {
+    font-size: 1.8rem;
+    margin-top: 1.1rem;
+  }
 `
 const ErrorLink = styled(Link)`
   color: ${colors.primary};
@@ -24,6 +33,11 @@ const ErrorLink = styled(Link)`
   font-size: 18px;
   display: inline-block;
   margin-top: 18.2rem;
+  @media screen and (max-width: 768px) {
+    font-size: 1.4rem;
+    margin-top: 13.3rem;
+    margin-bottom: 23.5rem;
+  }
 `
 
 const Error = () => {

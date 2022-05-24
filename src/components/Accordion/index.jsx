4a98fa939bba08fbf9856c134f12e0eebffc9428
TimotheGonin <gonin.timothe@gmail.com>
2022-05-24 15:25:20 +0200

    ✨ feat: add dynamic Accordion

--- src/components/Accordion/index.jsx
+++ src/components/Accordion/index.jsx
@@ -1,9 +1,13 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
 import AccordionIcon from '../../assets/arrow_back.svg'
 
-const AccordionWrapper = styled.div`
-  margin: 3.1rem 2rem;
+const AccordionContainer = styled.div`
+  margin: 3.1rem 0;
+  width: 100%;
+
+  max-width: 102.3rem;
   display: flex;
   align-items: center;
   flex-direction: column;
@@ -22,8 +26,8 @@ const AccordionWrapper = styled.div`
 `
 
 const AccordionButton = styled.button`
+  cursor: pointer;
   border: none;
-  max-width: 102.3rem;
   width: 100%;
   height: 4.7rem;
   display: flex;
@@ -36,6 +40,14 @@ const AccordionButton = styled.button`
   color: white;
   font-weight: 500;
   font-size: 24px;
+  img.close {
+    transform: rotate(0deg);
+    transition: 0.3s;
+  }
+  img.open {
+    transform: rotate(180deg);
+    transition: 0.3s;
+  }
   @media screen and (max-width: 576px) {
     height: 3rem;
     padding-left: 1.5rem;
@@ -49,18 +61,26 @@ const AccordionButton = styled.button`
 `
 
 const Accordion = () => {
+  const [diplayed, handleAccordion] = useState(false)
+
   return (
-    <AccordionWrapper>
-      <AccordionButton>
+    <AccordionContainer>
+      <AccordionButton onClick={() => handleAccordion(diplayed ? false : true)}>
         Fiabilité
-        <img src={AccordionIcon} alt="Accordion toggler" />
+        <img
+          src={AccordionIcon}
+          alt="Accordion toggler"
+          className={diplayed ? 'open' : 'close'}
+        />
       </AccordionButton>
-      <p>
-        La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
-        comportement discriminatoire ou de perturbation du voisinage entraînera
-        une exclusion de notre plateforme.
-      </p>
-    </AccordionWrapper>
+      {diplayed && (
+        <p>
+          La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+          comportement discriminatoire ou de perturbation du voisinage
+          entraînera une exclusion de notre plateforme.
+        </p>
+      )}
+    </AccordionContainer>
   )
 }
 export default Accordion
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 import Banner from '../../components/Banner'
 import Accordion from '../../components/Accordion'
+
+const AccordionsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 2rem;
+`
+
 const About = () => {
   return (
     <React.Fragment>
       <Banner />
-      <Accordion />
+      <AccordionsWrapper>
+        <Accordion />
+      </AccordionsWrapper>
     </React.Fragment>
   )
 }

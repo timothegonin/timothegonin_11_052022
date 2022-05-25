f03b30ddc23d27402cec0f83d3f4880fa6acd435
TimotheGonin <gonin.timothe@gmail.com>
2022-05-25 14:15:25 +0200

    🚧 add rental info MARKUP

--- src/pages/Rental/index.jsx
+++ src/pages/Rental/index.jsx
@@ -1,4 +1,50 @@
+import React from 'react'
+
 const Rental = () => {
-  return <h2>Rental</h2>
+  return (
+    <React.Fragment>
+      <aside>
+        <img src="" alt="" />
+        <button></button>
+        <button></button>
+        <p></p>
+      </aside>
+      <section>
+        <div>
+          <h1>Cozy loft on the Canal Saint-Martin</h1>
+          <h2>Paris, Île-de-France</h2>
+          <div>
+            <div>Cozy</div>
+            <div>Canal</div>
+            <div>Paris 10</div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h4>Alexande</h4>
+            <h3>Dumas</h3>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <span>
+              <i></i>
+            </span>
+            <span>
+              <i></i>
+            </span>
+            <span>
+              <i></i>
+            </span>
+            <span>
+              <i></i>
+            </span>
+            <span>
+              <i></i>
+            </span>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
+  )
 }
 export default Rental

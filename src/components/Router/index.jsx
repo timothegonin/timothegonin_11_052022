66f777fb44c55e0efe312f578e6dde7162fb0209
timotheG <gonin.timothe@gmail.com>
2023-12-02 18:08:31 +0100

    route and gh-pages link

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -9,7 +9,7 @@ const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/timothegonin_11_052022.git" element={<Home />} />
+      <Route path="/timothegonin_11_052022" element={<Home />} />
       <Route path="rental/:id" element={<Rental />} />
       <Route path="about" element={<About />} />
       <Route path="error-404" element={<Error />} />
